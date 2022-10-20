dc2396d5b74187a82728b1da170c9ede11cecef7
peterhase <peterhase@google.com>
2022-10-20 03:52:11 +0000

    bug fix

--- experiments/evaluate.py
+++ experiments/evaluate.py
@@ -580,7 +580,7 @@ if __name__ == "__main__":
     if '6B' in model_name:
         central_layers = list(range(0, 28, 4)) + [5, 27]
         num_layers = 28
-    if alg_name == 'FT' and 1 in window_sizes:
+    if alg_name == 'FT' and 1 in window_sizes and not args.use_noised_subject:
         central_layers = [-1] + central_layers
     if args.edit_layer > -2:
         central_layers = [args.edit_layer]
