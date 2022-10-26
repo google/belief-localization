005243d823f193dafbb351a779dbb5015e91c931
peterhase <peterhase@google.com>
2022-10-26 21:18:51 +0000

    bug fix

--- experiments/evaluate.py
+++ experiments/evaluate.py
@@ -472,7 +472,6 @@ def main(
                 # splice uncorrupted hidden_states into corrupted_hidden_states where they are restored. automatically broadcast across num_noise_samples dimension
                 hidden_state_supervision = corrupted_hidden_states
                 hidden_state_supervision[:,:,last_subj_idx,:] = clean_hidden_states[:,:,last_subj_idx,:]
-                # import pdb; pdb.set_trace()
                 # model(**gen_batch, output_hidden_states=True).hidden_states
             else:
                 hidden_state_supervision = None
@@ -495,7 +494,7 @@ def main(
                   return_orig_weights=True,
                   num_noise_samples=num_noise_samples,
                   prior_prob=prior_prob,
-                  hidden_state_supervision=hidden_state_supervision
+                  hidden_state_supervision=hidden_state_supervision,
                   **args_conserve_memory,
               )
             exec_time = time.time() - start
