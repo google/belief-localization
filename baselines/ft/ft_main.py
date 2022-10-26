63f6fd115f08b3cb27f61b102c14a487c08508a9
peterhase <peterhase@google.com>
2022-10-26 22:06:40 +0000

    bug fix

--- baselines/ft/ft_main.py
+++ baselines/ft/ft_main.py
@@ -161,8 +161,10 @@ def execute_ft(
             if args.weight_based_tracing:
                 hidden_states = outputs.hidden_states
                 hidden_states = torch.stack([hidden_states[layer+1] for layer in hparams.layers], dim=0)
-                loss_mat = (hidden_states - hidden_state_supervision)**2
-                per_tok_loss = loss_mat.sum(0).sum(0).sum(-1)
+                # loss_mat = (hidden_states - hidden_state_supervision)**2
+                # per_tok_loss = loss_mat.sum(0).sum(0).sum(-1)
+                loss_mat = (hidden_states[0,0:,:] - hidden_state_supervision[0,0,:,:])**2
+                per_tok_loss = loss_mat.sum(-1)
                 # loss = per_tok_loss.sum()
                 loss = per_tok_loss[e_range[1]-1]
                 # if it % 10 == 0:
