2a09ac17924347fbea252a14f09f0dddf4860ad3
peterhase <peterhase@google.com>
2022-11-02 02:03:09 +0000

    bug fix

--- experiments/tracing.py
+++ experiments/tracing.py
@@ -372,7 +372,7 @@ def causal_tracing_loop(experiment_name, task_name, split_name, mt, eval_data,
       # potentially skip if exists
       if not overwrite:
         _model_name = model_name.split('/')[-1]
-        save_path = f"{BASE_DIR}/results/{_model_name}/cases/{experiment_name}_{data_point_id}_{kind}.csv"
+        save_path = f"{BASE_DIR}/results/{_model_name}/traces/{experiment_name}_{data_point_id}_{kind}.csv"
         if os.path.exists(save_path):
           if printing:
             print(f"skipping batch {batch_num}, point {data_point_id}, as it is already written")
@@ -394,11 +394,11 @@ def causal_tracing_loop(experiment_name, task_name, split_name, mt, eval_data,
       # plot and save results (both results_dict, for their plotting code, and the results_df, for ours)
       if save_plots:
         plot_name = f"{experiment_name}_plot{data_point_id}_{kind}.pdf"
-        save_path = os.path.join(f'{BASE_DIR}/results/plots', plot_name) if plot_name else None 
+        save_path = os.path.join(f'{BASE_DIR}/results/{_model_name}/traces', plot_name) if plot_name else None 
         print(f"saving plot at {save_path}")
         _model_name = model_name.split('/')[-1]
         plot_trace_heatmap(results_dict, show_plot=show_plots, savepdf=save_path, modelname=_model_name)
-        save_path = f"{BASE_DIR}/results/{_model_name}/cases/{experiment_name}_{data_point_id}_{kind}.npz"
+        save_path = f"{BASE_DIR}/results/{_model_name}/traces/{experiment_name}_{data_point_id}_{kind}.npz"
         if printing:
           print(f"saving results at {save_path}")
         np.savez(save_path, results_dict)
