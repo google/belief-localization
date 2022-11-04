5b289a3f7d7e4391d39b4efe4d465a119394c2ba
peterhase <peterhase@google.com>
2022-11-04 18:31:24 +0000

    bug fix

--- experiments/evaluate.py
+++ experiments/evaluate.py
@@ -72,7 +72,7 @@ def get_override_hparams(args, window_size, central_layer, alg_name):
         if args.fact_erasure:
             return_dict['norm_constraint'] = 5e-5
         if args.fact_amplification:
-            return_dict['norm_constraint'] = 1e-5
+            return_dict['norm_constraint'] = 5e-5
         else:
             return_dict['norm_constraint'] = 1e-4
   # hack for applying ROME to multiple 3 layers
